e3afab15944716f57460ea6a9a58b2898d2a392b
koalaa13 <nooshe981@gmail.com>
2020-03-17 22:50:43 +0300

    Created a new Rep for Java-Advanced

--- Java-Codes/Java-Advanced-2020/ru.itmo.rain.maksimov/implementor/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * This package contains implementation of interface {@link info.kgeorgiy.java.advanced.implementor.JarImpler},
- * used for creating default implementation of classes and interfaces.
- */
-
-package ru.itmo.rain.maksimov.implementor;
